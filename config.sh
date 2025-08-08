3de6f4f94f7c3e2c2a7b43f83bc1da3748678f7d
William <vimscientist69@gmail.com>
2025-08-08 14:11:53 -0600

    fix(config.sh): improved accuracy of echo statement: configuring both swiftlint and swiftformat

--- config.sh
+++ config.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo "Configuring SwiftLint"
+echo "Configuring SwiftLint and SwiftFormat"
 
 ln -s ./.dotfiles/.swiftlint.yml .swiftlint.yml
 ln -s ./.dotfiles/.swiftformat-base .swiftformat
